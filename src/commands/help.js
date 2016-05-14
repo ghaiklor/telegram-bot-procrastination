400aad478dbdf2cf157729e777e627a2d73b71e8
Eugene Obrezkov <ghaiklor@gmail.com>
2016-05-14 13:04:47 +0300

    feat: add more information in /help

--- src/commands/help.js
+++ src/commands/help.js
@@ -5,6 +5,17 @@ const MESSAGES = require('../common/messages');
 module.exports = bot => {
   bot.onText(/\/help/, message => {
     const fromId = message.from.id;
-    bot.sendMessage(fromId, MESSAGES.START_MESSAGE);
+
+    bot.sendMessage(fromId, `
+Ask me a question in the following format:
+
+Can ... ?
+Does ... ?
+Do ... ?
+Is ... ?
+Am ... ?
+Are ... ?
+Will ... ?
+`);
   });
 };
--- src/common/messages.js
+++ src/common/messages.js
@@ -1,5 +1,5 @@
 "use strict";
 
 module.exports = {
-  START_MESSAGE: `You can ask me any question you want to.`
+  START_MESSAGE: `You can ask me any question you want to. Type /help for more information.`
 };
